8a7760eb97f68f4d1044ae7ec8681b2fd97a1b9c
kangb3 <kangb3@TACOMA.UW.EDU>
2018-03-03 23:58:37 -0800

    MTcode

---
+++ ContosoUniversity/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public abstract class Address
+    {
+
+       [RegularExpression(@"^[a-zA-Z0-9._%+-]+@contoso\.edu$", ErrorMessage = "This is an invalid email address.")]
+        public string Email { get; set; }
+    }
+
+
+}
